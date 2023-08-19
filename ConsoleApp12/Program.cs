175f4f6f29dd60aa3fa7909776f3a50fbf96d7a3
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-19 20:19:12 +0200

    added more lines

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -31,6 +31,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Monday is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Monday+1) + "st day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Monday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Monday+1) + "st day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Tuesday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Tuesday+1) + "nd day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Wednesday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Wednesday+1) + "rd day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Thursday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Thursday+1) + "th day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Friday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Friday+1) + "th day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Saturday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Saturday+1) + "th day of the week ");
+        Console.WriteLine(DaysOfTheWeek.Sunday+" is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Sunday+1) + "th day of the week ");
+        
+        
     }
 }
