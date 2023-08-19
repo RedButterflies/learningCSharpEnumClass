2c4472e294e24d4efc444d22ad8e8fad97ff4fc1
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-19 20:08:52 +0200

    added two enums, representing days of the week and months

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,2 +1,36 @@
 ﻿//the Enum class
+enum DaysOfTheWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
 
+enum Months
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Monday is the: " +Convert.ToInt16( (int)DaysOfTheWeek.Monday+1) + "st day of the week ");
+    }
+}
